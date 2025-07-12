ab67265728c71e4cd48163427977074f9a6213db
Ole Voß <ole.voss@posteo.de>
2025-07-12 15:02:10 +0200

    save

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+use ruduino::cores::current::port;
+use ruduino::Pin;
+
+#[no_mangle]
+pub extern fn main() -> ! {
+    port::B5::set_output();
+
+    loop {
+        port::B5::set_high();
+
+        ruduino::delay::delay_ms(1000);
+
+        port::B5::set_low();
+
+        ruduino::delay::delay_ms(1000);
+    }
+}
+
+#[panic_handler]
+fn panic(_panic: &PanicInfo) -> ! {
+    loop {}
 }
